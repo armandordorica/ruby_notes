503d1ca49d7990c1c2920b86efb824b234cdba74
Armando-Ordorica <armandordorica@gmail.com>
2016-08-07 17:15:26 -0400

    Inheritance and modules

--- user.rb
+++ user.rb
@@ -1,4 +1,10 @@
+module Destructable
+  def destroy(anyobject)
+    puts "I will destroy the object"
+end
+
 class User
+  include Destructable
   attr_accessor :name, :email
 
   #this method is called every time you create a new object
@@ -19,6 +25,25 @@ class User
   # def set_name=(name)
   #   @name = name
   # end
+
+
+def self.identify_yourself
+  puts "hey i am a class method"
+  
+end
+
+end
+
+class Buyer < User
+  def run
+    puts "Hey I'm not running and I'm in buyer class"
+  end
+end
+
+class Seller < User
+end
+
+class Admin < User
 end
 
 user = User.new("Armando", "armandordorica@gmail.com")
@@ -33,3 +58,17 @@ puts "My user's name is #{user.name} and the associated email is #{user.email}"
 user.name = "John"
 user.email = "john@example.com"
 puts "My user's new name is #{user.name} and the associated email is #{user.email}"
+
+
+buyer1 = Buyer.new("John Doe", "johndoe@example.com")
+buyer1.run
+
+seller1 = Seller.new("John Doe1", "johndoe1@example.com")
+seller1.run
+
+admin1 = Admin.new("John Doe2", "johndoe2@example.com")
+admin1.run
+
+puts Buyer.ancestors
+
+User.identify_yourself
